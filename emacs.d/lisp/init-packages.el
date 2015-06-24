4b84597b7c75eda95baf4d517b8f621e9b50332b
Czipperz <czipperz@gmail.com>
2015-06-23 23:32:44 -0500

    Its gnu not melpa. Derp

--- emacs.d/lisp/init-packages.el
+++ emacs.d/lisp/init-packages.el
@@ -2,7 +2,7 @@
 (require 'package)
 (push '("marmalade" . "http://marmalade-repo.org/packages/") package-archives)
 (push '("melpa" . "http://melpa.milkbox.net/packages/") package-archives)
-(push '("melpa" . "http://elpa.gnu.org/packages/") package-archives)
+(push '("gnu-elpa" . "http://elpa.gnu.org/packages/") package-archives)
 (package-initialize)
 
 (provide 'init-packages)
