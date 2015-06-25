4e849ccedaaebb758c24c3e81d3cc12ad4be46cc
Czipperz <czipperz@gmail.com>
2015-06-25 14:54:41 -0500

    < and > move intuitively now

--- lisp/init-yasnippet.el
+++ lisp/init-yasnippet.el
@@ -2,7 +2,7 @@
 
 (yas-global-mode 1)
 
-(global-set-key (kbd "C-<") 'yas-next-field-or-maybe-expand)
-(global-set-key (kbd "C->") 'yas-prev-field)
+(global-set-key (kbd "C-<") 'yas-prev-field)
+(global-set-key (kbd "C->") 'yas-next-field-or-maybe-expand)
 
 (provide 'init-yasnippet)
