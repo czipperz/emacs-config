21fad5ac2202ed0bf76577f0908868ba366808f5
Czipperz <czipperz@gmail.com>
2015-06-25 02:17:01 -0500

    Remapped to C-<,C-> because return broke

--- lisp/init-yasnippet.el
+++ lisp/init-yasnippet.el
@@ -2,9 +2,7 @@
 
 (yas-global-mode 1)
 
-(cond (window-system
-       (global-set-key (kbd "<return>") 'newline)
-       (global-set-key (kbd "C-m") 'yas-next-field-or-maybe-expand)
-       (global-set-key (kbd "C-S-m") 'yas-prev-field)))
+(global-set-key (kbd "C-<") 'yas-next-field-or-maybe-expand)
+(global-set-key (kbd "C->") 'yas-prev-field)
 
 (provide 'init-yasnippet)
