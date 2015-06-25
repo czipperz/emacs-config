d5b25d538495cb9928e24c011da4aadfbfc0fdd5
Czipperz <czipperz@gmail.com>
2015-06-25 00:09:32 -0500

    C-m works much better

--- lisp/init-yasnippet.el
+++ lisp/init-yasnippet.el
@@ -2,7 +2,9 @@
 
 (yas-global-mode 1)
 
-(global-set-key (kbd "C-m") 'yas-next-field-or-maybe-expand)
-(global-set-key (kbd "C-S-m") 'yas-prev-field)
+(cond (window-system
+       (global-set-key (kbd "<return>") 'newline)
+       (global-set-key (kbd "C-m") 'yas-next-field-or-maybe-expand)
+       (global-set-key (kbd "C-S-m") 'yas-prev-field)))
 
 (provide 'init-yasnippet)
