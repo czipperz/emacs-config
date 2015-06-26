b81383f048823c1277366a2c5a26d34dda18c60f
Czipperz <czipperz@gmail.com>
2015-06-26 15:22:48 -0500

    Use loop for requires

--- init.el
+++ init.el
@@ -1,26 +1,26 @@
 ; Custom scripts `require`
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
 
-; Init scripts
-(require 'init-packages)
-(require 'init-markdown)
-(require 'init-powerline)
-(require 'init-mappings)
-(require 'init-yasnippet)
-(require 'init-functions)
-
-; Other requires
-(require 'darcula-theme)
-(require 'magit)
-(require 'helm-config)
-(require 'auto-complete)
-(require 'smartparens-config)
+(dolist (i '(;; Init scripts
+	     init-packages
+	     init-markdown
+	     init-powerline
+	     init-mappings
+	     init-yasnippet
+	     init-functions
+	     ;; Other requires
+	     darcula-theme
+	     magit
+	     helm-config
+	     auto-complete
+	     smartparens-config))
+  (require i))
 
 (custom-set-variables ; Your init file should only contain one of these
- '(custom-safe-themes ("ea489f6710a3da0738e7dbdfc124df06a4e3ae82f191ce66c2af3e0a15e99b90"
-		       "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0"
-		       "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4"
-		       default)))
+ '(custom-safe-themes '("ea489f6710a3da0738e7dbdfc124df06a4e3ae82f191ce66c2af3e0a15e99b90"
+			"a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0"
+			"8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4"
+			default)))
 (custom-set-faces    ; Your init file should only contain one of these
  )
 
