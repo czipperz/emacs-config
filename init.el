ba46458b779b1236dd031dccc704cbfbbae07a9b
Czipperz <czipperz@gmail.com>
2015-06-25 23:20:32 -0500

    Better comments and comment functions required

--- init.el
+++ init.el
@@ -7,6 +7,7 @@
 (require 'init-powerline)
 (require 'init-mappings)
 (require 'init-yasnippet)
+(require 'init-functions)
 
 ; Other requires
 (require 'darcula-theme)
@@ -15,28 +16,28 @@
 (require 'auto-complete)
 (require 'smartparens-config)
 
-(custom-set-variables ;; Your init file should only contain one of these
+(custom-set-variables ; Your init file should only contain one of these
  '(custom-safe-themes (quote ("ea489f6710a3da0738e7dbdfc124df06a4e3ae82f191ce66c2af3e0a15e99b90"
 			      "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0"
 			      "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4"
 			      default)))
  )
-(custom-set-faces ;; Your init file should only contain one of these
+(custom-set-faces    ; Your init file should only contain one of these
  )
 
-;(load-theme 'solarized-dark) ; load theme here
+;;(load-theme 'solarized-dark) ; load theme here
 
 (set-default-font "Meslo LG S DZ-12")
 
-;; Magit
+;; Magit - Git inside emacs is awesome -> `C-c s' or `M-x magit-status'
 (setq magit-last-seen-setup-instructions "1.4.0") ; Hide message
 (autoload 'magit-status "magit" nil t)
 
-;; Helm - auto complete ftw
+;; Auto Complete menus
 (helm-mode 1)
 (helm-autoresize-mode 1)
 
-;; Auto Complete - too stronk
+;; Auto Complete for text
 (ac-config-default)
 
 ;; Auto match parens (highlight)
