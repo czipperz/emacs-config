ddfe00cae257960846d683a0067272c805170da9
Czipperz <czipperz@gmail.com>
2015-06-26 11:50:35 -0500

    Don't need to unset before set

--- lisp/init-mappings.el
+++ lisp/init-mappings.el
@@ -1,28 +1,21 @@
 ;;; C-c commands
 ;; Show unsaved edits
-(global-unset-key (kbd "C-c d"))
 (global-set-key (kbd "C-c d") 'diff-buffer-with-current-file)
 ;; Show git status
-(global-unset-key (kbd "C-c s"))
 (global-set-key (kbd "C-c s") 'magit-status)
 ;; Terminal
-(global-unset-key (kbd "C-c t"))
 (global-set-key (kbd "C-c t") 'term)
 
 
 ;; Move current line (where Point is) down one
-(global-unset-key (kbd "M-n"))
 (global-set-key (kbd "M-n") 'move-line-down)
 ;; Move current line (where Point is) down one
-(global-unset-key (kbd "M-p"))
 (global-set-key (kbd "M-p") 'move-line-up)
 
 ;; rebind `C-,' to vim style `o'
 ;; rebind `C-.' to vim styoe `O'
-(global-unset-key (kbd "C-,"))
 (global-set-key (kbd "C-,")        'vim-style-o)
 
-(global-unset-key (kbd "C-."))
 (global-set-key (kbd "C-.")        'vim-style-capital-o)
 
 ;; Unbind `C-z' as its annoying
@@ -30,9 +23,7 @@
 
 ;; `C-a' bound to beginning of text (vim ^)
 ;; `C-S-a' bound to beginning of line (vim 0)
-(global-unset-key (kbd "C-a"))
 (global-set-key (kbd "C-a") 'beginning-of-line-text)
-(global-unset-key (kbd "C-S-a"))
 (global-set-key (kbd "C-S-a") 'move-beginning-of-line)
 
 ;; `C-k' acts `dd' instead of until end of line
