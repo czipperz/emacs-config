048938f92390033a26b1ed077e5efd844c1dbd1c
Czipperz <czipperz@gmail.com>
2015-06-25 23:36:48 -0500

    Refactored functions out of init-mappings

--- lisp/init-functions.el
+++ lisp/init-functions.el
@@ -1,3 +1,53 @@
+(defun diff-buffer-with-current-file () "Shows differences in the current buffer since the last save"
+       (interactive)
+       (diff-buffer-with-file))
+
+
+(defun move-line-down () "Moves the current line down by one"
+       (interactive)
+       (let ((col (current-column)))
+	 (forward-line 1)
+	 (transpose-lines 1)
+	 (forward-line -1)
+	 (forward-char col)))
+(defun move-line-up () "Moves the current line up by one"
+       (interactive)
+       (let ((col (current-column)))
+	 (transpose-lines 1)
+	 (forward-line -2)
+	 (forward-char col)))
+
+
+(defun vim-style-o () "Puts a new line after the current one and puts the Point there"
+       (interactive)
+       (move-end-of-line nil)
+       (newline)
+       (indent-for-tab-command))
+(defun vim-style-capital-o () "Puts a new line before the current one and puts the Point there"
+       (interactive)
+       (forward-line -1)
+       (vim-style-o))
+
+
+(defun kill-start-of-line () "Kills to the start of the line"
+       (interactive)
+       (kill-line 0))
+(defun kill-start-of-text () "Kills to the start of the text"
+       (interactive)
+       (let ((curr (point)))
+	 (beginning-of-line-text)
+	 (kill-region curr (point))))
+
+
+;; Because the prefix should be the group
+(defun package-list () "Display a list of packages.
+This first fetches the updated list of packages before
+displaying, unless a prefix argument NO-FETCH is specified.
+The list is displayed in a buffer named `*Packages*'."
+       (interactive)
+       (list-packages))
+
+
 (defun comment-line () "Comments the current line"
        (interactive)
        (save-excursion
@@ -12,5 +62,6 @@
 	 (set-mark (point))
 	 (move-end-of-line 1)
 	 (uncomment-region (mark) (point))))
+
 
 (provide 'init-functions)
--- lisp/init-mappings.el
+++ lisp/init-mappings.el
@@ -2,9 +2,6 @@
 ;; Show unsaved edits
 (global-unset-key (kbd "C-c d"))
 (global-set-key (kbd "C-c d") 'diff-buffer-with-current-file)
-(defun diff-buffer-with-current-file () "Shows differences in the current buffer since the last save"
-       (interactive)
-       (diff-buffer-with-file))
 ;; Show git status
 (global-unset-key (kbd "C-c s"))
 (global-set-key (kbd "C-c s") 'magit-status)
@@ -16,39 +13,17 @@
 ;; Move current line (where Point is) down one
 (global-unset-key (kbd "M-n"))
 (global-set-key (kbd "M-n") 'move-line-down)
-(defun move-line-down () "Moves the current line down by one"
-       (interactive)
-       (let ((col (current-column)))
-	 (forward-line 1)
-	 (transpose-lines 1)
-	 (forward-line -1)
-	 (forward-char col)))
 ;; Move current line (where Point is) down one
 (global-unset-key (kbd "M-p"))
 (global-set-key (kbd "M-p") 'move-line-up)
-(defun move-line-up () "Moves the current line up by one"
-       (interactive)
-       (let ((col (current-column)))
-	 (transpose-lines 1)
-	 (forward-line -2)
-	 (forward-char col)))
 
 ;; rebind `C-,' to vim style `o'
 ;; rebind `C-.' to vim styoe `O'
 (global-unset-key (kbd "C-,"))
 (global-set-key (kbd "C-,")        'vim-style-o)
-(defun vim-style-o () "Puts a new line after the current one and puts the Point there"
-       (interactive)
-       (move-end-of-line nil)
-       (newline)
-       (indent-for-tab-command))
 
 (global-unset-key (kbd "C-."))
 (global-set-key (kbd "C-.")        'vim-style-capital-o)
-(defun vim-style-capital-o () "Puts a new line before the current one and puts the Point there"
-       (interactive)
-       (forward-line -1)
-       (vim-style-o))
 
 ;; Unbind `C-z' as its annoying
 (global-unset-key (kbd "C-z"))
@@ -69,27 +44,12 @@
 ;; `C-:' kills to start of line
 (global-set-key (kbd "C-;") 'kill-start-of-text)
 (global-set-key (kbd "C-:") 'kill-start-of-line)
-(defun kill-start-of-line () "Kills to the start of the line"
-       (interactive)
-       (kill-line 0))
-(defun kill-start-of-text () "Kills to the start of the text"
-       (interactive)
-       (let ((curr (point)))
-	 (beginning-of-line-text)
-	 (kill-region curr (point))))
 
-;; Because the prefix should be the group
-(defun package-list () "Display a list of packages.
-This first fetches the updated list of packages before
-displaying, unless a prefix argument NO-FETCH is specified.
-The list is displayed in a buffer named `*Packages*'."
-       (interactive)
-       (list-packages))
-
+;; Hyper modifiers that make life easier
 (global-set-key (kbd "H-c") 'comment-line)
-(gloabl-set-key (kbd "H-u") 'uncomment-line)
+(global-set-key (kbd "H-u") 'uncomment-line)
 (global-set-key (kbd "H-q") 'delete-window)
-(gloabl-set-key (kbd "H-v") 'split-window-right)
+(global-set-key (kbd "H-v") 'split-window-right)
 (global-set-key (kbd "H-h") 'split-window-below)
 
 (provide 'init-mappings)
