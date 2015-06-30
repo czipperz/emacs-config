543b36e85e86e83d6b0faaa3eb40c5117f019742
Czipperz <czipperz@gmail.com>
2015-06-30 14:53:54 -0500

    No more yasnippet because it's annoying

--- lisp/init-yasnippet.el
+++ lisp/init-yasnippet.el
@@ -1,8 +1,5 @@
-(require 'yasnippet)
+;(require 'yasnippet)
 
-(yas-global-mode 1)
-
-(global-set-key (kbd "C-<") 'yas-prev-field)
-(global-set-key (kbd "C->") 'yas-next-field-or-maybe-expand)
+;(yas-global-mode 1)
 
 (provide 'init-yasnippet)
