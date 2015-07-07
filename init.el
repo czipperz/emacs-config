db53f852a872db139f8b639d9b9055c5a0711f28
Czipperz <czipperz@gmail.com>
2015-07-07 12:58:44 -0700

    We won

--- init.el
+++ init.el
@@ -31,7 +31,6 @@
 			"8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4"
 			default)))
 (custom-set-faces    ; Your init file should only contain one of these
- '(rainbow-delimiters-depth-1-face )
  )
 
 ;;(load-theme 'solarized-dark) ; load theme here
