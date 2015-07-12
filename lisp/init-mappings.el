d81eb121775c3223f482e1724ef1ed2a16a2e8f0
Czipperz <czipperz@gmail.com>
2015-07-12 00:50:59 -0700

    Added notes about what doesn't work in shells

--- lisp/init-mappings.el
+++ lisp/init-mappings.el
@@ -1,5 +1,5 @@
-(global-set-key (kbd "C-,") 'vim-style-o)
-(global-set-key (kbd "C-.") 'vim-style-capital-o)
+(global-set-key (kbd "C-,") 'vim-style-o) ; note that this doesn't work in shells because they are dumb
+(global-set-key (kbd "C-.") 'vim-style-capital-o) ; note that this doesn't work in shells because they are dumb
 
 (global-unset-key (kbd "C-z"))		;This would sleep emacs so unbound it
 (global-unset-key (kbd "C-x C-z"))
@@ -11,7 +11,7 @@
 (global-set-key (kbd "C-S-a") 'move-beginning-of-line)
 
 (global-set-key (kbd "C-k") 'kill-whole-line)
-(global-set-key (kbd "C-S-k") 'kill-line)
+(global-set-key (kbd "C-S-k") 'kill-line) ; note that this doesn't work in shells because they are dumb
 
 (global-set-key (kbd "C-;") 'kill-start-of-text)
 (global-set-key (kbd "C-:") 'kill-start-of-line)
