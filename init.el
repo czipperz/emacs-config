62660839a5fff82cc14c03763a242acd702f9606
Czipperz <czipperz@gmail.com>
2015-07-12 00:17:33 -0700

    custom-set-variables reformatted by computer

--- init.el
+++ init.el
@@ -29,11 +29,14 @@
 	     highlight-current-line
 	     )) (require i))
 
-(custom-set-variables ; Your init file should only contain one of these
- '(custom-safe-themes '("ea489f6710a3da0738e7dbdfc124df06a4e3ae82f191ce66c2af3e0a15e99b90"
-			"a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0"
-			"8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4"
-			default)))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("ea489f6710a3da0738e7dbdfc124df06a4e3ae82f191ce66c2af3e0a15e99b90" "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
