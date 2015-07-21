dde770930ca96f706fdf613c09dfea8f47664aaa
Czipperz <czipperz@gmail.com>
2015-07-20 20:28:14 -0700

    Moved Hyper keys to C-z keys

--- lisp/init-mappings.el
+++ lisp/init-mappings.el
@@ -6,19 +6,18 @@
 (global-set-key (kbd "C-z d") 'diff-buffer-with-current-file)
 (global-set-key (kbd "C-z s") 'magit-status)
 (global-set-key (kbd "C-z t") 'term)
+(global-set-key (kbd "C-z c") 'comment-line)
+(global-set-key (kbd "C-z u") 'uncomment-line)
+(global-set-key (kbd "C-z q") 'delete-window)
+(global-set-key (kbd "C-z v") 'split-window-right)
+(global-set-key (kbd "C-z h") 'split-window-below)
+(global-set-key (kbd "C-z o") 'delete-other-windows)
 
 (global-set-key (kbd "C-k") 'kill-whole-line)
 (global-set-key (kbd "C-S-k") 'kill-line) ; note that this doesn't work in shells because they are dumb
 
 (global-set-key (kbd "C-;") 'kill-start-of-text)
 (global-set-key (kbd "C-:") 'kill-start-of-line)
-
-(global-set-key (kbd "H-c") 'comment-line)
-(global-set-key (kbd "H-u") 'uncomment-line)
-(global-set-key (kbd "H-q") 'delete-window)
-(global-set-key (kbd "H-v") 'split-window-right)
-(global-set-key (kbd "H-h") 'split-window-below)
-(global-set-key (kbd "H-o") 'delete-other-windows)
 
 (global-set-key (kbd "M-<") 'my/beginning-of-buffer)
 (global-set-key (kbd "M->") 'my/end-of-buffer)
