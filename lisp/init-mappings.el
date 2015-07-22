a60bc10fb8c544cc945091d2cf22d54ee9c5b48d
Czipperz <czipperz@gmail.com>
2015-07-22 12:41:10 -0700

    `C-z C-e' moves to end of line instead of beginning of next

--- lisp/init-mappings.el
+++ lisp/init-mappings.el
@@ -13,7 +13,7 @@
 (global-set-key (kbd "C-z h") 'split-window-below)
 (global-set-key (kbd "C-z o") 'delete-other-windows)
 (global-set-key (kbd "C-z C-a") 'beginning-of-visual-line)
-(global-set-key (kbd "C-z C-e") 'end-of-visual-line)
+(global-set-key (kbd "C-z C-e") (lambda () (interactive) (end-of-visual-line) (backward-char)))
 
 (global-set-key (kbd "C-k") 'kill-whole-line)
 (global-set-key (kbd "C-S-k") 'kill-line) ; note that this doesn't work in shells because they are dumb
