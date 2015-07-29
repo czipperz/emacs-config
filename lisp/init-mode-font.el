6652d1776b05dced58667462c232d89c323949ae
Czipperz <czipperz@gmail.com>
2015-07-29 10:48:10 -0700

    `(lambda () (interactive)` looks like `Λ`

--- lisp/init-mode-font.el
+++ lisp/init-mode-font.el
@@ -3,6 +3,10 @@
                            (0 (progn (compose-region (match-beginning 1) (match-end 1) ?λ) nil)))))
 
 (font-lock-add-keywords 'emacs-lisp-mode
+                        '(("(\\(lambda () (interactive.*?)\\)"
+                           (0 (progn (compose-region (match-beginning 1) (match-end 1) ?Λ) nil)))))
+
+(font-lock-add-keywords 'emacs-lisp-mode
                         '(("(\\(lambda\\)\\>"
                            (0 (progn (compose-region (match-beginning 1) (match-end 1) ?λ) nil)))))
 
