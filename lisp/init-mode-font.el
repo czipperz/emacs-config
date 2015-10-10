ebc48c4b791a9e828d07169f543dcd0c5134cf7d
Czipperz <czipperz@gmail.com>
2015-10-10 16:25:38 -0700

    Don't show haskell lambdas as lambda symbol

--- lisp/init-mode-font.el
+++ lisp/init-mode-font.el
@@ -1,7 +1,3 @@
-(font-lock-add-keywords 'haskell-mode
-                        '(("(\\(\\\\\\)"
-                           (0 (progn (compose-region (match-beginning 1) (match-end 1) ?λ) nil)))))
-
 (font-lock-add-keywords 'emacs-lisp-mode
                         '(("(\\(lambda () (interactive)\\)"
                            (0 (progn (compose-region (match-beginning 1) (match-end 1) ?Λ) nil)))))
