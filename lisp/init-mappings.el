36a2a9a89566c09f1b0c4efaf583423a84ad12fc
Czipperz <czipperz@gmail.com>
2015-10-10 16:13:25 -0700

    Abstract over custom prefix and change it from ``C-z`` to ``C-s``

--- lisp/init-mappings.el
+++ lisp/init-mappings.el
@@ -1,24 +1,85 @@
+(defvar my-prefix "C-s")
+
+(global-unset-key (kbd my-prefix))
+(global-set-key (kbd (concat my-prefix " d")) 'diff-buffer-with-current-file)
+(global-set-key (kbd (concat my-prefix " s")) 'magit-status)
+(global-set-key (kbd (concat my-prefix " t")) 'term)
+(global-set-key (kbd (concat my-prefix " c")) 'comment-line)
+(global-set-key (kbd (concat my-prefix " u")) 'uncomment-line)
+(global-set-key (kbd (concat my-prefix " C-c")) 'comment-region)
+(global-set-key (kbd (concat my-prefix " C-u")) 'uncomment-region)
+(global-set-key (kbd (concat my-prefix " q")) 'delete-window)
+(global-set-key (kbd (concat my-prefix " v")) 'split-window-right)
+(global-set-key (kbd (concat my-prefix " h")) 'split-window-below)
+(global-set-key (kbd (concat my-prefix " o")) 'delete-other-windows)
+(global-set-key (kbd (concat my-prefix " C-f")) 'ido-find-file)
+(global-set-key (kbd (concat my-prefix " C-a")) 'beginning-of-visual-line)
+(global-set-key (kbd (concat my-prefix " C-e")) 'my/end-of-visual-line)
+(add-hook 'sh-mode-hook '(lambda () (local-set-key (kbd (concat my-prefix " p")) 'insert-perl-regexp)))
+
+(global-set-key (kbd (concat my-prefix " SPC")) 'fix-indentation)
+
+(global-unset-key (kbd (concat my-prefix " j")))
+(global-set-key (kbd (concat my-prefix " j g")) 'java-get/set)
+(global-set-key (kbd (concat my-prefix " j a")) 'java-get/set-&-align)
+(global-set-key (kbd (concat my-prefix " j v")) 'java-line-private)
+(global-set-key (kbd (concat my-prefix " j m")) 'java-function-public)
+(global-set-key (kbd (concat my-prefix " j ;")) 'java-fix-semicolons)
+(global-set-key (kbd (concat my-prefix " j r")) 'java-fix-return)
+(global-set-key (kbd (concat my-prefix " j w")) 'java-single-method-wrap)
+(global-set-key (kbd (concat my-prefix " j s")) 'java-assign-to-set)
+(global-set-key (kbd (concat my-prefix " j C-s")) 'groovy-with-to-set)
+(global-set-key (kbd (concat my-prefix " j e")) 'groovy-each-to-for)
+(global-set-key (kbd (concat my-prefix " j (")) 'groovy-space-to-parens)
+(global-set-key (kbd (concat my-prefix " j n")) 'c-prog-sep-semicolon)
+(global-set-key (kbd (concat my-prefix " j C-g")) (lambda () (interactive) (insert "get") (my/capitalize-word) (insert "()")))
+
+(global-set-key (kbd (concat my-prefix " r")) 'align-regexp)
+
+;; (global-set-key (kbd (concat my-prefix " f")) 'forward-find)
+;; (global-set-key (kbd (concat my-prefix " b")) 'backward-find)
+
+(global-set-key (kbd (concat my-prefix " C-o")) 'ace-window)
+(setq aw-keys '(?a ?s ?e ?r ?t ?h ?u ?i ?o ?p))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 (global-set-key (kbd "C-x C-b") 'ibuffer-list-buffers)
 (global-set-key (kbd "C-x b")   'helm-buffers-list)
 
-(global-unset-key (kbd "C-z"))		;This would sleep emacs so unbound it
-(global-unset-key (kbd "C-x C-z"))
-(global-set-key (kbd "C-z d") 'diff-buffer-with-current-file)
-(global-set-key (kbd "C-z s") 'magit-status)
-(global-set-key (kbd "C-z t") 'term)
-(global-set-key (kbd "C-z c") 'comment-line)
-(global-set-key (kbd "C-z u") 'uncomment-line)
-(global-set-key (kbd "C-z C-c") 'comment-region)
-(global-set-key (kbd "C-z C-u") 'uncomment-region)
-(global-set-key (kbd "C-z q") 'delete-window)
-(global-set-key (kbd "C-z v") 'split-window-right)
-(global-set-key (kbd "C-z h") 'split-window-below)
-(global-set-key (kbd "C-z o") 'delete-other-windows)
-(global-set-key (kbd "C-z C-f") 'ido-find-file)
-(global-set-key (kbd "C-z C-a") 'beginning-of-visual-line)
-(global-set-key (kbd "C-z C-e") 'my/end-of-visual-line)
-(add-hook 'sh-mode-hook '(lambda () (local-set-key (kbd "C-z p") 'insert-perl-regexp)))
+(global-unset-key (kbd "C-x C-z")) ;This would sleep emacs so unbound it
+
+(global-set-key [C-M-tab] 'clang-format-region)
+
+(add-hook 'diff-mode-hook '(lambda () (local-set-key (kbd "q") (lambda () (interactive) (delete-window)))))
+
+(global-set-key (kbd "C-$") 'ispell-buffer)
+
+(global-set-key (kbd "M-'")  'insert-single-quotes)
+(global-set-key (kbd "M-\"") 'insert-double-quotes)
+(global-set-key (kbd "C-{")  'backward-paragraph)
+(global-set-key (kbd "C-}")  'forward-paragraph)
+(global-set-key (kbd "M-{")  'insert-curly-brackets)
+(global-set-key (kbd "M-[")  'insert-square-brackets)
+
+(global-set-key (kbd "C-=") 'er/expand-region)
+(global-set-key (kbd "C-+") 'er/contract-region)
+
+(global-set-key (kbd "C-t")   'my/transpose-chars)
+(global-set-key (kbd "M-t")   'my/transpose-words)
+(global-set-key (kbd "C-M-t") 'my/transpose-sexps)
 
 (global-set-key (kbd "C-k") 'kill-whole-line)
 (global-set-key (kbd "C-S-k") 'kill-line) ; note that this doesn't work in shells because they are dumb
@@ -85,48 +146,5 @@
 (global-set-key (kbd "C-x 8 g x")   "χ")  ;chi
 (global-set-key (kbd "C-x 8 g X")   "Ξ")  ;XI
 (global-set-key (kbd "C-x 8 g z")   "ζ")  ;zeta
-
-(global-set-key (kbd "C-z SPC") 'fix-indentation)
-
-(global-unset-key (kbd "C-z j"))
-(global-set-key (kbd "C-z j g") 'java-get/set)
-(global-set-key (kbd "C-z j a") 'java-get/set-&-align)
-(global-set-key (kbd "C-z j v") 'java-line-private)
-(global-set-key (kbd "C-z j m") 'java-function-public)
-(global-set-key (kbd "C-z j ;") 'java-fix-semicolons)
-(global-set-key (kbd "C-z j r") 'java-fix-return)
-(global-set-key (kbd "C-z j w") 'java-single-method-wrap)
-(global-set-key (kbd "C-z j s") 'java-assign-to-set)
-(global-set-key (kbd "C-z j C-s") 'groovy-with-to-set)
-(global-set-key (kbd "C-z j e") 'groovy-each-to-for)
-(global-set-key (kbd "C-z j (") 'groovy-space-to-parens)
-(global-set-key (kbd "C-z j n") 'c-prog-sep-semicolon)
-(global-set-key (kbd "C-z j C-g") (lambda () (interactive) (insert "get") (my/capitalize-word) (insert "()")))
-
-(global-set-key (kbd "C-z r") 'align-regexp)
-
-(add-hook 'diff-mode-hook '(lambda () (local-set-key (kbd "q") (lambda () (interactive) (delete-window)))))
-
-(global-set-key (kbd "C-$") 'ispell-buffer)
-
-(global-set-key (kbd "M-'")  'insert-single-quotes)
-(global-set-key (kbd "M-\"") 'insert-double-quotes)
-(global-set-key (kbd "C-{")  'backward-paragraph)
-(global-set-key (kbd "C-}")  'forward-paragraph)
-(global-set-key (kbd "M-{")  'insert-curly-brackets)
-(global-set-key (kbd "M-[")  'insert-square-brackets)
-
-(global-set-key (kbd "C-z C-o") 'ace-window)
-(setq aw-keys '(?a ?s ?e ?r ?t ?h ?u ?i ?o ?p))
-
-(global-set-key (kbd "C-=") 'er/expand-region)
-(global-set-key (kbd "C-+") 'er/contract-region)
-
-(global-set-key (kbd "C-t")   'my/transpose-chars)
-(global-set-key (kbd "M-t")   'my/transpose-words)
-(global-set-key (kbd "C-M-t") 'my/transpose-sexps)
-
-(global-set-key (kbd "C-z f") 'forward-find)
-(global-set-key (kbd "C-z b") 'backward-find)
 
 (provide 'init-mappings)
