78ccb190ed6cc20fb1e2169605bf42fe66d27b29
Czipperz <czipperz@gmail.com>
2016-06-15 19:17:55 -0700

    Backup files into `~/.emacs.d/backups/`

--- init.el
+++ init.el
@@ -9,6 +9,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(auth-source-save-behavior nil)
+ '(backup-directory-alist (quote ((".*" . "~/.emacs.d/backups/"))))
  '(blink-matching-paren nil)
  '(custom-safe-themes
    (quote
