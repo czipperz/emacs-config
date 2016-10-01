cd6b262210f430cbae70ef08c3602ccf844b3bfc
Czipperz <czipperz@gmail.com>
2016-09-30 21:38:03 -0700

    defun write-file-as-user: allow reopening file as different user, saving changes.

--- lisp/init-functions.el
+++ lisp/init-functions.el
@@ -56,6 +56,113 @@ that is not 0.  If ARG is less than 0, it will call comment-region."
                              buffer-file-name)))
   (shell-command (concat "doctest " filename)))
 
+(defsubst strip-tramp-username (filename)
+  "Remove `/SERVICE:USER@MACHINE:' preceding FILENAME."
+  (replace-tramp-username filename))
+
+(defun replace-tramp-username (filename &optional username)
+  "Replace USER with USERNAME in `/SERVICE:USER@MACHINE:' preceding FILENAME.
+
+Order of operation goes top to bottom, breaking when finding an
+appropriate action:
+
+If USERNAME is nil, instead remove the prefix completely, doing
+nothing if there is no prefix.
+
+If USERNAME works as a prefix, replace the prefix with it.
+
+If FILENAME has a prefix, replace the `USER' in the prefix with USERNAME.
+
+If the prefix on FILENAME doesn't exist, prefix `/USERNAME::' to
+the result (which will translate to `/sudo:USERNAME@localhost:')."
+  (let (action)
+    (setq action
+          (if (or (null username) (string-empty-p username))
+              1
+            ;; USERNAME is not nil
+            (with-temp-buffer (insert username)
+                                (goto-char (point-min))
+                                (if (or (looking-at-p "/.*:.*@.*:")
+                                        (looking-at-p "/.*::"))
+                                    ;; USERNAME has prefix
+                                    2
+                                  ;; USERNAME has no prefix
+                                  3))))
+    (with-temp-buffer
+      (insert filename)
+      (goto-char (point-min))
+      (save-match-data (if (looking-at "/.*:\\(.*\\)@.*:")
+                           ;; prefix does exist
+                           (case action
+                             ;; remove prefix and exit
+                             ((1)
+                              (delete-region (match-beginning 0)
+                                             (match-end 0)))
+                             ;; remove prefix and add USERNAME as one
+                             ((2)
+                              (delete-region (match-beginning 0)
+                                             (match-end 0))
+                              (goto-char (point-min))
+                              (insert username))
+                             ;; substitute USER with USERNAME in
+                             ;; `/SERVICE:USER@MACHINE:'
+                             ((3)
+                              (delete-region (match-beginning 1)
+                                             (match-end 1))
+                              (goto-char (match-beginning 1))
+                              (insert username)))
+                         ;; prefix doesn't exist
+                         (case action
+                           ;; do nothing
+                           ((1))
+                           ;; add USERNAME as a prefix
+                           ((2)
+                            (insert username))
+                           ;; /USERNAME::
+                           ((3)
+                            (insert "/" username "::")))))
+      (buffer-substring-no-properties (point-min) (point-max)))))
+
+(defun write-file-as-user (filename user)
+  "Write current buffer into file FILENAME as USER via TRAMP.
+
+This will transfer into the new file, just like `write-file', and
+future saves will also use USER."
+  (interactive "bWrite file: \nsUser to save as: ")
+  (write-file (replace-tramp-username filename user)))
+
+(defun sudo-write-file (filename)
+  "Write current buffer into file FILENAME as root via TRAMP.
+
+This will transfer into the new file, just like `write-file', and
+future saves will also use root."
+  (interactive "bWrite file: ")
+  (write-file-as-user filename "sudo"))
+
+;; (defun save-buffer-as-user (user)
+;;   "Save current buffer in visited file, as USER via TRAMP, if it has been modified.
+
+;; This will cause future saves to also use USER."
+;;   (interactive "sUser to save as: ")
+;;   (when (or (null buffer-file-name)
+;;             (string-equal buffer-file-name ""))
+;;     (setq buffer-file-name (read-string "Buffer name (previously unnamed): ")))
+;;   (let ((filename (format "/%s::%s" user buffer-file-name)))
+;;     (set-visited-file-name filename t)
+;;     ;; Make buffer writable if file is writable.
+;;     (and buffer-file-name
+;;          (file-writable-p buffer-file-name)
+;;          (setq buffer-read-only nil))
+;;     (save-buffer)
+;;     (vc-refresh-state)))
+
+;; (defun sudo-save-buffer ()
+;;   "Save current buffer in visited file, as root via TRAMP, if it has been modified.
+
+;; This will cause future saves to also use root."
+;;   (interactive)
+;;   (save-buffer-as-user "sudo"))
+
 (defun c++-to-u8 ()
   "Change a character or string at point (or after) to be u8.
 
