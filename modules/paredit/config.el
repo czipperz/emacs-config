41f9925c10964092f755ecbdd487c6887c7183e0
Chris Gregory <czipperz@gmail.com>
2017-07-08 00:28:44 -0500

    Unbind keys in paredit that conflict with other modes (mc, company)

--- modules/paredit/config.el
+++ modules/paredit/config.el
@@ -2,6 +2,9 @@
 
 (define-key paredit-mode-map (kbd "M-r") nil)
 (define-key paredit-mode-map (kbd "C-r") #'paredit-raise-sexp)
+(define-key paredit-mode-map (kbd "C-j") nil)
+(define-key paredit-mode-map (kbd "C-M-p") nil)
+(define-key paredit-mode-map (kbd "C-M-n") nil)
 
 (add-hook 'emacs-lisp-mode-hook #'enable-paredit-mode)
 (add-hook 'emacs-lisp-mode-hook #'eldoc-mode)
