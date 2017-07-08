a23822a5ac08357a679307400f7015d8aae08d37
Chris Gregory <czipperz@gmail.com>
2017-07-08 00:23:08 -0500

    No disabled commands

--- modules/general/config.el
+++ modules/general/config.el
@@ -8,3 +8,13 @@
 
 (setq vc-follow-symlinks t)
 (setq enable-recursive-minibuffers t)
+
+(put 'erase-buffer 'disabled nil)
+(put 'narrow-to-region 'disabled nil)
+(put 'set-goal-column 'disabled nil)
+(put 'narrow-to-page 'disabled nil)
+(put 'upcase-region 'disabled nil)
+(put 'scroll-left 'disabled nil)
+(put 'Info-edit 'disabled nil)
+(put 'dired-find-alternate-file 'disabled nil)
+(put 'downcase-region 'disabled nil)
