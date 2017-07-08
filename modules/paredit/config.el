2d082245eaa401da7fab819b074f16f723ca1587
Chris Gregory <czipperz@gmail.com>
2017-07-08 00:26:53 -0500

    Enable eldoc;  Have it and paredit in minibuffer

--- modules/paredit/config.el
+++ modules/paredit/config.el
@@ -1,5 +1,9 @@
 (require 'paredit)
 
-(add-hook 'emacs-lisp-mode-hook #'enable-paredit-mode)
 (define-key paredit-mode-map (kbd "M-r") nil)
 (define-key paredit-mode-map (kbd "C-r") #'paredit-raise-sexp)
+
+(add-hook 'emacs-lisp-mode-hook #'enable-paredit-mode)
+(add-hook 'emacs-lisp-mode-hook #'eldoc-mode)
+(add-hook 'eval-expression-minibuffer-setup-hook #'enable-paredit-mode)
+(add-hook 'eval-expression-minibuffer-setup-hook #'eldoc-mode)
