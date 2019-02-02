0740159fa41f05cb67793d517dc887016598304f
Czipperz <czipperz@gmail.com>
2019-02-02 12:31:20 -0500

    Speed up company auto complete

--- modules/company/config.el
+++ modules/company/config.el
@@ -1,6 +1,9 @@
 (require 'company)
 (require 'company-cmake)
 
+(setq company-minimum-prefix-length 1)
+(setq company-idle-delay 0.05)
+
 (global-company-mode)
 (defun my/setup-company ()
   "Bind `C-j' to `company-complete'."
