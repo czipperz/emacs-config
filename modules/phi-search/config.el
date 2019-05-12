6a71c80241b734fd4a4e93159cc8ef3060d4c7d6
Chris Gregory <czipperz@gmail.com>
2019-05-12 16:39:00 -0500

    Fix phi-search to use M-p and M-n

--- modules/phi-search/config.el
+++ modules/phi-search/config.el
@@ -4,3 +4,6 @@
 (global-set-key (kbd "C-M-%") 'phi-replace-query)
 (global-set-key (kbd "C-M-s") 'phi-search)
 (global-set-key (kbd "C-M-r") 'phi-search-backward)
+
+(define-key phi-search-default-map (kbd "M-p") #'ivy-previous-history-element)
+(define-key phi-search-default-map (kbd "M-n") #'ivy-next-history-element)
